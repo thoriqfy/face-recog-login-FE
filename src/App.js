c335a27ec0b24e507c9bd723308500aa51058f7c
thoriqfy <thor.iq.yahya01@gmail.com>
2022-06-01 17:24:43 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Webcam from 'react-webcam';
+import WebcamCapture from './components/webcam.js';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <div className='head'>
+          <h2>Live Face Recognition run on the Web</h2>
+        </div>
+        <WebcamCapture/>
+      </div>
+      )
+  }
+}
+
+export default App;
