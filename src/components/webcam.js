1746a2401ee2763e1d37b1a9ee78b257afc19ea2
thoriqfy <thor.iq.yahya01@gmail.com>
2022-06-02 01:48:45 +0700

    update routing

--- src/components/webcam.js
+++ src/components/webcam.js
@@ -15,7 +15,7 @@ const WebcamCapture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
     console.log(`imageSrc = ${imageSrc}`)
                 //for deployment, you should put your backend url / api
-    axios.post('http://127.0.0.1:5000/api', {data : imageSrc})
+    axios.post('https://face-backend-sopodata.herokuapp.com/api', {data : imageSrc})
     	  .then(res => {
       	  console.log(`response = ${res.data}`)
       	  setName(res.data)
